d3f3c6098be599090d7073b325ab0a091cacecb4
IEDC CEAL <72986429+iedcattingal@users.noreply.github.com>
2020-10-17 14:02:45 +0530

    Update program2.c

--- program2.c
+++ program2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int avg(int a, int b)
+{int avg;
+avg=a+b/2
+return avg;
+}
+int avg(int c, int d, int e)
+{int avg;
+avg=c+d+e/2
+return avg;
+}
+int main()
+{
+    int ans;
+    
+    ans = avg(4,5);
+    printf("Average of the numbers is: %d",answer);
+
+    
+}
 
