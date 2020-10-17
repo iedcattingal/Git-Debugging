0015d0161138e1fab334fedd982de15cd9218121
IAMNITHIN <73012078+IAMNITHIN@users.noreply.github.com>
2020-10-17 14:48:16 +0530

    Update program1.c

--- program1.c
+++ program1.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
-
-void main()
+#include<math.h>
+int main()
 {
 int n,r,flag=3,s=0;
 printf("Enter a three digit number");
-scanf("%d",n);
-
-while(n>0);
+scanf("%d",&n);
+while(n>0)
 {
 r=n%10;
 flag=-1;
 s=s+r*pow(flag,10);
-n=n/10:
+n=n/10;
 }
-print("%d",&s);
+printf("%d",s);
+}
 
-}
 
